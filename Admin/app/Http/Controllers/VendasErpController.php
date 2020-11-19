49b8f9c5416e6e5659d9c36cb60e49c95534f946
yuziem14 <yuri.fnunes@gmail.com>
2020-11-19 15:00:32 -0300

    feat: adiciona valores totais da pesquisa na tabela

--- Admin/app/Http/Controllers/VendasErpController.php
+++ Admin/app/Http/Controllers/VendasErpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Request;
 use DB;
+use App\MeioCaptura;
 use App\StatusConciliacaoModel;
 use App\GruposClientesModel;
 use App\AdquirentesModel;
@@ -12,20 +13,24 @@ class VendasErpController extends Controller{
 
   public function vendaserp(){
     $adquirentes = DB::table('cliente_operadora')
-    ->join('adquirentes', 'cliente_operadora.COD_ADQUIRENTE', 'adquirentes.CODIGO')
-    ->select('adquirentes.*')
-    ->where('cliente_operadora.COD_CLIENTE', '=', session('codigologin'))
-    ->distinct('COD_ADQUIRENTE')
-    ->get();
+      ->join('adquirentes', 'cliente_operadora.COD_ADQUIRENTE', 'adquirentes.CODIGO')
+      ->select('adquirentes.*')
+      ->where('cliente_operadora.COD_CLIENTE', '=', session('codigologin'))
+      ->distinct('COD_ADQUIRENTE')
+      ->get();
 
-    $meio_captura = DB::table('meio_captura')->get();
-    $status_conciliacao = StatusConciliacaoModel::where('CODIGO', '!=', 4)->orderBy('STATUS_CONCILIACAO', 'ASC')->get();
-    $grupos_clientes = GruposClientesModel::where('COD_CLIENTE', '=', session('codigologin'))->get();
+    $meio_captura = MeioCaptura::all();
+    $status_conciliacao = StatusConciliacaoModel::where('CODIGO', '!=', 4)
+      ->orderBy('STATUS_CONCILIACAO', 'ASC')
+      ->get();
+    $grupos_clientes = GruposClientesModel::where('COD_CLIENTE', '=', session('codigologin'))
+      ->get();
 
-    return view('vendas.vendaserp')->with('adquirentes', $adquirentes)
-    ->with('grupos_clientes', $grupos_clientes)
-    ->with('meio_captura', $meio_captura)
-    ->with('status_conciliacao', $status_conciliacao);
+    return view('vendas.vendaserp')
+      ->with('adquirentes', $adquirentes)
+      ->with('grupos_clientes', $grupos_clientes)
+      ->with('meio_captura', $meio_captura)
+      ->with('status_conciliacao', $status_conciliacao);
   }
 
   public function buscarVendasErp() {
@@ -37,220 +42,68 @@ class VendasErpController extends Controller{
     $conciliacao = Request::input('status_conciliacao');
     $quantidadePorPagina = Request::input('por_pagina', 10);
     $quantidadePorPagina = in_array($quantidadePorPagina, $quantidadesPermitidas) ? $quantidadePorPagina : 10;
-
-    $vendas = DB::table('vendas_erp')
-    ->join('modalidade', 'vendas_erp.COD_MODALIDADE', '=', 'modalidade.CODIGO')
-    ->leftJoin('produto_web', 'vendas_erp.COD_PRODUTO', '=', 'produto_web.CODIGO')
-    ->leftJoin('meio_captura', 'vendas_erp.COD_MEIO_CAPTURA', '=', 'meio_captura.CODIGO')
-    ->leftJoin('status_conciliacao', 'vendas_erp.COD_STATUS_CONCILIACAO', '=', 'status_conciliacao.CODIGO')
-    ->select('vendas_erp.*', 'vendas_erp.CODIGO as COD', 'modalidade.*', 'produto_web.*', 'meio_captura.DESCRICAO as MEIOCAPTURA', 'status_conciliacao.STATUS_CONCILIACAO')
-    ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
-    // ->where(function($query) {
-    //   if(Request::only('array') != null){
-    //     $empresas = Request::only('array');
-    //     foreach ($empresas['array'] as $cnpj) {
-    //       $query->orWhere('CNPJ', '=', $cnpj);
-    //     }
-    //   }
-    // })
-    ->where(function($query) {
-      if(Request::only('arrayStatusConciliacao') != null){
-        $status_conciliacao = Request::only('arrayStatusConciliacao');
-        foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
-          $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
+    
+    $query = DB::table('vendas_erp')
+      ->join('modalidade', 'vendas_erp.COD_MODALIDADE', '=', 'modalidade.CODIGO')
+      ->leftJoin('produto_web', 'vendas_erp.COD_PRODUTO', '=', 'produto_web.CODIGO')
+      ->leftJoin('meio_captura', 'vendas_erp.COD_MEIO_CAPTURA', '=', 'meio_captura.CODIGO')
+      ->leftJoin('status_conciliacao', 'vendas_erp.COD_STATUS_CONCILIACAO', '=', 'status_conciliacao.CODIGO')
+      ->select('vendas_erp.*', 'vendas_erp.CODIGO as COD', 'modalidade.*', 'produto_web.*', 'meio_captura.DESCRICAO as MEIOCAPTURA', 'status_conciliacao.STATUS_CONCILIACAO')
+      ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
+      ->where(function($query) {
+        if(Request::only('arrayStatusConciliacao') != null){
+          $status_conciliacao = Request::only('arrayStatusConciliacao');
+          foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
+            $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
+          }
         }
-      }
-    })
-    ->where(function($query) {
-      if(Request::only('arrayAdquirentes') != null){
-        $adquirentes = Request::only('arrayAdquirentes');
-        foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
-          $query->orWhere('COD_OPERADORA', '=', $adquirente);
+      })
+      ->where(function($query) {
+        if(Request::only('arrayAdquirentes') != null){
+          $adquirentes = Request::only('arrayAdquirentes');
+          foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
+            $query->orWhere('COD_OPERADORA', '=', $adquirente);
+          }
         }
-      }
-    })
-    ->where(function($query) {
-      if(Request::only('arrayMeioCaptura') != null){
-        $meiocaptura = Request::only('arrayMeioCaptura');
-        foreach ($meiocaptura['arrayMeioCaptura'] as $mcaptura) {
-          $query->orWhereNull('COD_MEIO_CAPTURA')->orWhere('COD_MEIO_CAPTURA', '=', $mcaptura);
+      })
+      ->where(function($query) {
+        if(Request::only('arrayMeioCaptura') != null){
+          $meiocaptura = Request::only('arrayMeioCaptura');
+          foreach ($meiocaptura['arrayMeioCaptura'] as $mcaptura) {
+            $query->orWhereNull('COD_MEIO_CAPTURA')->orWhere('COD_MEIO_CAPTURA', '=', $mcaptura);
+          }
         }
-      }
-    })
-    ->where(function($query) {
-      if(Request::only('data_inicial') != null){
-        $data_inicial = Request::only('data_inicial');
-        $data_final = Request::only('data_final');
-        $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);
+      })
+      ->where(function($query) {
+        if(Request::only('data_inicial') != null){
+          $data_inicial = Request::only('data_inicial');
+          $data_final = Request::only('data_final');
+          $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);
+        }
+      })
+      ->orderBy('DATA_VENDA');
 
-        // $query->where('DATA_VENDA', '>=', '2020-08-01')->where('DATA_VENDA', '<=', '2020-09-05');
-      }
-    })
-    ->orderBy('DATA_VENDA')
-    ->paginate($quantidadePorPagina);
+    $liquidezTotalPacela = $query->get()->sum('VALOR_LIQUIDO_PARCELA');
+    $totalVendas = $query->get()->sum('TOTAL_VENDA');
+    $paginaVendas = $query->paginate($quantidadePorPagina);
 
-    // $val_bruto = DB::table('vendas_erp')
-    // ->join('modalidade', 'vendas_erp.CODIGO_MODALIDADE', '=', 'modalidade.CODIGO')
-    // ->selectRaw('sum(VALOR_BRUTO) as val_bruto')
-    // ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
-    // // ->where(function($query) {
-    // //   if(Request::only('array') != null){
-    // //     $empresas = Request::only('array');
-    // //     foreach ($empresas['array'] as $cnpj) {
-    // //       $query->orWhere('CNPJ', '=', $cnpj);
-    // //     }
-    // //   }
-    // // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayStatusConciliacao') != null){
-    //     $status_conciliacao = Request::only('arrayStatusConciliacao');
-    //     foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
-    //       $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayAdquirentes') != null){
-    //     $adquirentes = Request::only('arrayAdquirentes');
-    //     foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
-    //       $query->orWhere('COD_OPERADORA', '=', $adquirente);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('data_inicial') != null){
-    //     $data_inicial = Request::only('data_inicial');
-    //     $data_final = Request::only('data_final');
-    //     $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);      }
-    // })
-    // ->first();
-    //
-    // $val_liquido = DB::table('vendas_erp')
-    // ->join('modalidade', 'vendas_erp.CODIGO_MODALIDADE', '=', 'modalidade.CODIGO')
-    // ->selectRaw('sum(VALOR_LIQUIDO) as val_liquido')
-    // ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
-    // // ->where(function($query) {
-    // //   if(Request::only('array') != null){
-    // //     $empresas = Request::only('array');
-    // //     foreach ($empresas['array'] as $cnpj) {
-    // //       $query->orWhere('CNPJ', '=', $cnpj);
-    // //     }
-    // //   }
-    // // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayStatusConciliacao') != null){
-    //     $status_conciliacao = Request::only('arrayStatusConciliacao');
-    //     foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
-    //       $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayAdquirentes') != null){
-    //     $adquirentes = Request::only('arrayAdquirentes');
-    //     foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
-    //       $query->orWhere('COD_OPERADORA', '=', $adquirente);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('data_inicial') != null){
-    //     $data_inicial = Request::only('data_inicial');
-    //     $data_final = Request::only('data_final');
-    //     $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);      }
-    // })
-    // ->first();
-    //
-    // $val_taxa_soma = DB::table('vendas_erp')
-    // ->join('modalidade', 'vendas_erp.CODIGO_MODALIDADE', '=', 'modalidade.CODIGO')
-    // ->selectRaw('sum(VALOR_TAXA) as val_taxa')
-    // ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
-    // // ->where(function($query) {
-    // //   if(Request::only('array') != null){
-    // //     $empresas = Request::only('array');
-    // //     foreach ($empresas['array'] as $cnpj) {
-    // //       $query->orWhere('CNPJ', '=', $cnpj);
-    // //     }
-    // //   }
-    // // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayStatusConciliacao') != null){
-    //     $status_conciliacao = Request::only('arrayStatusConciliacao');
-    //     foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
-    //       $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayAdquirentes') != null){
-    //     $adquirentes = Request::only('arrayAdquirentes');
-    //     foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
-    //       $query->orWhere('COD_OPERADORA', '=', $adquirente);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('data_inicial') != null){
-    //     $data_inicial = Request::only('data_inicial');
-    //     $data_final = Request::only('data_final');
-    //     $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);      }
-    // })
-    // ->first();
-    //
-    // $val_taxa_percent = DB::table('vendas_erp')
-    // ->join('modalidade', 'vendas_erp.CODIGO_MODALIDADE', '=', 'modalidade.CODIGO')
-    // ->selectRaw('sum(OUTRAS_DESPESAS) as val_taxa_percent')
-    // ->where('vendas_erp.COD_CLIENTE', '=', session('codigologin'))
-    // // ->where(function($query) {
-    // //   if(Request::only('array') != null){
-    // //     $empresas = Request::only('array');
-    // //     foreach ($empresas['array'] as $cnpj) {
-    // //       $query->orWhere('CNPJ', '=', $cnpj);
-    // //     }
-    // //   }
-    // // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayStatusConciliacao') != null){
-    //     $status_conciliacao = Request::only('arrayStatusConciliacao');
-    //     foreach($status_conciliacao['arrayStatusConciliacao'] as $status_conciliacaoo) {
-    //       $query->orWhere('COD_STATUS_CONCILIACAO', '=', $status_conciliacaoo);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('arrayAdquirentes') != null){
-    //     $adquirentes = Request::only('arrayAdquirentes');
-    //     foreach ($adquirentes['arrayAdquirentes'] as $adquirente) {
-    //       $query->orWhere('COD_OPERADORA', '=', $adquirente);
-    //     }
-    //   }
-    // })
-    // ->where(function($query) {
-    //   if(Request::only('data_inicial') != null){
-    //     $data_inicial = Request::only('data_inicial');
-    //     $data_final = Request::only('data_final');
-    //     $query->whereBetween('DATA_VENDA', [$data_inicial['data_inicial'], $data_final['data_final']]);      }
-    // })
-    // ->first();
+    $dados = [
+        'vendas' => $paginaVendas,
+        'totais' => [
+          'TOTAL_VENDAS' => $totalVendas,
+          'LIQUIDEZ_TOTAL_PARCELA' => $liquidezTotalPacela,
+        ]
+    ];
 
-    $qtde_registros = $vendas->count();
-
-    // $val_taxas = $val_bruto->val_bruto - $val_liquido->val_liquido;
-    session()->put('vendas_erp', $vendas);
-
-    // $modalidades = ModalidadesModel::orderBy('DESCRICAO', 'ASC')->get();
+    
     $adquirentes = AdquirentesModel::orderBy('ADQUIRENTE', 'ASC')->get();
     $status_conciliacao = StatusConciliacaoModel::where('CODIGO', '!=', 4)->orderBy('STATUS_CONCILIACAO', 'ASC')->get();
     $grupos_clientes = GruposClientesModel::where('COD_CLIENTE', '=', session('codigologin'))->get();
-    // $val_liquido = number_format($val_liquido->val_liquido, 2,",",".");
-    // $val_bruto = number_format($val_bruto->val_bruto, 2,",",".");
-    // $val_taxa_soma = number_format($val_taxa_soma->val_taxa, 2,",",".");
-    // $val_taxa_percent = number_format($val_taxa_percent->val_taxa_percent, 2, '.', '');
-    // $val_taxas = number_format($val_taxas, 2,",",".");
+    
+    session()->put('vendas_erp', $paginaVendas);
+    $dados = json_encode($dados);
 
-    $vendas = json_encode([$vendas]);
-
-    return $vendas;
+    return $dados;
   }
 
   public function downloadTable(){
@@ -259,6 +112,6 @@ class VendasErpController extends Controller{
 
     $pdf = \PDF::loadView('vendas.tabela_vendas', compact('vendas'));
     return $pdf->setPaper('A4', 'landscape')
-    ->download('prev_pag.pdf');
+              ->download('prev_pag.pdf');
   }
 }
