b34645bfc95ec5e8ac9ab99c36a91e05bd3a0118
yuziem14 <yuri.fnunes@gmail.com>
2020-12-07 16:49:36 -0300

    chore: implementacao de utilitarios para filtrar modais

--- Admin/public/assets/js/lib/modal-filters.js
+++ Admin/public/assets/js/lib/modal-filters.js
@@ -11,27 +11,68 @@ ModalFilter.prototype._convertStringAsArray = function(arrayAsString) {
   return arrayValues;
 }
 
-ModalFilter.prototype.onInputHandler = function(event, callback = () => {}) {
+ModalFilter.prototype._toggleVisibility = function(item, isVisible = true) {
+  if(isVisible) {
+    item.style.removeProperty('display');
+  } else {
+    item.style.display = 'none';
+  }
+}
+
+ModalFilter.prototype._toggleVisibilityAll = function(items, isVisible = true) {
+  [...items].forEach(item => {
+    this._toggleVisibility(item, isVisible);
+  });
+}
+
+ModalFilter.prototype.hideAll = function(name) {
+  const items = document.querySelectorAll(`*[data-filter-item-container=${name}]`);
+
+  this._toggleVisibilityAll(items, false);
+}
+
+ModalFilter.prototype.showAll = function(name) {
+  const items = document.querySelectorAll(`*[data-filter-item-container=${name}]`);
+
+  this._toggleVisibilityAll(items, true);
+}
+
+ModalFilter.prototype._onInputHandler = function(event) {
   const fieldInput = event.target;
   const valueToFilter = fieldInput.value;
   const groupName = fieldInput.dataset.filterGroup;
   const group = this.groups[groupName];
 
-  const selectorPrefix = `*[data-filter-group="${groupName}"]`;
+  if(!valueToFilter) {
+    this.showAll(groupName);
+    return;
+  }
+
+  this.hideAll(groupName);
+
+  const selectorPrefix = `*[data-filter-item-container=${groupName}]`;
   const querySelector = group.fields.map(field => {
-    return `${selectorPrefix}[data-filter-${field}]*="${valueToFilter}"`;
+    return `${selectorPrefix}[data-filter-${field}*="${valueToFilter}" i]`;
   }).join(', ');
 
   const filteredElements = document.querySelectorAll(querySelector);
 
-  if(callback && typeof callback === 'function') {
-    callback();
-  }
+  this._toggleVisibilityAll(filteredElements, true);
 }
 
-ModalFilter.prototype._registerHandler = function(name) {}
+ModalFilter.prototype._registerHandler = function(name, callback = () => {}) {
+  const { filterInput } = this.groups[name];
 
-ModalFilter.prototype.addGroup = function(name) {
+  filterInput.addEventListener('input', (event) => {
+    this._onInputHandler(event);
+
+    if(callback && typeof callback === 'function') {
+      callback(event);
+    }
+  })
+}
+
+ModalFilter.prototype.addGroup = function(name, callback = () => {}) {
   const filterInput = document.querySelector(`input[data-filter-group="${name}"]`);
   const fieldsAsString = filterInput.dataset.filterFields;
   const fields = this._convertStringAsArray(fieldsAsString);
@@ -43,4 +84,7 @@ ModalFilter.prototype.addGroup = function(name) {
       fields,
     }
   }
+
+  this._registerHandler(name, callback);
+  return this;
 }
