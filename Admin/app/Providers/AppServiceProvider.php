56468b85e80a4ec2c09f4b5b04590f609b92a32d
yuziem14 <yuri.fnunes@gmail.com>
2021-02-03 16:37:11 -0300

    chore: registra filtros de recebimentos

--- Admin/app/Providers/AppServiceProvider.php
+++ Admin/app/Providers/AppServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Filters\VendasErpFilter;
 use App\Filters\VendasFilter;
+use App\Filters\RecebimentosFilter;
 use App\Filters\VendasErpSubFilter;
 use App\Filters\VendasSubFilter;
+use App\Filters\RecebimentosSubFilter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,11 +25,17 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(VendasFilter::class, function ($app) {
             return new VendasFilter;
         });
+        $this->app->bind(RecebimentosFilter::class, function ($app) {
+            return new RecebimentosFilter;
+        });
         $this->app->bind(VendasErpSubFilter::class, function($app) {
             return new VendasErpSubFilter($app->make(VendasErpFilter::class));
         });
         $this->app->bind(VendasSubFilter::class, function($app) {
             return new VendasSubFilter($app->make(VendasFilter::class));
+        });
+        $this->app->bind(RecebimentosSubFilter::class, function($app) {
+            return new RecebimentosSubFilter($app->make(RecebimentosFilter::class));
         });
     }
 
