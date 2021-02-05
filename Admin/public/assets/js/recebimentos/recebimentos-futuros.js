97b687b8326a1dea5b8a5914f8df237931cc99c5
yuziem14 <yuri.fnunes@gmail.com>
2021-02-05 16:38:14 -0300

    chore: padronizacao e implementacao de funcionalidades de recebimentos futuros

---
+++ Admin/public/assets/js/recebimentos/recebimentos-futuros.js
@@ -0,0 +1,190 @@
+const checker = new Checker();
+const modalFilter = new ModalFilter();
+const formatter = new Formatter({
+  locale: 'pt-BR',
+  currencyOptions: {
+    type: 'BRL'
+  }
+});
+const searchForm = createSearchForm({
+  form: '#js-form-pesquisa',
+  inputs: ['_token', 'data_inicial', 'data_final'],
+  checker
+});
+const paymentsContainer = new PaymentsContainerProxy({
+  id: 'recebimentos-operadoras',
+  links: {
+    search: searchForm.get('form').dataset.urlBuscarRecebimentos,
+    filter: searchForm.get('form').dataset.urlFiltrarRecebimentos,
+  }
+});
+const tableRender = createTableRender({
+  table: '#js-tabela-recebimentos',
+  locale: 'pt-br',
+  formatter,
+});
+const boxes = getBoxes();
+
+checker.addGroups([
+  { name: 'empresa', options: { inputName: 'grupos_clientes' } },
+  { name: 'adquirente', options: { inputName: 'adquirentes' } },
+  { name: 'bandeira', options: { inputName: 'bandeiras' } },
+  { name: 'modalidade', options: { inputName: 'modalidades' } },
+  { name: 'domicilio-bancario', options: { inputName: 'domicilios_bancarios' } },
+]);
+
+modalFilter.addGroups([
+  'empresa',
+  'adquirente',
+  'domicilio-bancario'
+]);
+
+searchForm.onSubmit(async (event) => {
+  const resultadosDOM = document.querySelector('.resultados');
+
+  await paymentsContainer.search({
+    params: {
+      por_pagina: paymentsContainer.get('search').get('pagination').options.perPage,
+    },
+    body: { ...searchForm.serialize() },
+  });
+
+  tableRender.clearFilters();
+
+  if (resultadosDOM.classList.contains('hidden')) {
+    resultadosDOM.classList.remove('hidden');
+  }
+  window.scrollTo(0, document.querySelector('.resultados').offsetTop);
+});
+
+paymentsContainer.setupApi({
+  headers: {
+    'X-CSRF-TOKEN': searchForm.getInput('_token').value,
+    'Content-Type': 'application/json',
+  }
+});
+
+paymentsContainer.onEvent('beforeFetch', () => {
+  toggleElementVisibility('#js-loader');
+});
+
+paymentsContainer.onEvent('fetch', (payments) => {
+  toggleElementVisibility('#js-loader');
+  document.querySelector('#js-quantidade-registros').textContent = `(${payments.get('pagination').options.total || 0} registros)`;
+
+  tableRender.set('data', {
+    body: (payments.get('payments') || []),
+    footer: (payments.get('totals') || {}),
+  });
+  tableRender.render();
+  payments.get('pagination').render();
+});
+
+paymentsContainer.onEvent('search', (payments) => {
+  const totals = payments.get('totals');
+  updateBoxes(boxes, {
+    ...totals,
+    TOTAL_TAXA: (totals.TOTAL_TAXA || 0) * -1,
+    TOTAL_ANTECIPACAO: (totals.TOTAL_ANTECIPACAO || 0) * -1,
+    TOTAL_DESPESAS: (totals.TOTAL_DESPESAS || 0) * -1,
+  });
+});
+
+paymentsContainer.onEvent('fail', (err) => {
+  document.querySelector('#js-loader').classList.remove('hidden');
+  document.querySelector('#js-loader').classList.add('hidden');
+});
+
+paymentsContainer.setPaginationConfig({
+  paginationContainer: document.querySelector('#js-paginacao-recebimentos')
+},
+  async (page, pagination, event) => {
+    await buildRequest({
+      page,
+      por_pagina: pagination.options.perPage,
+    })
+    .get();
+  }
+);
+
+function buildRequest(params) {
+  let requestHandler = () => {};
+
+  if(paymentsContainer.get('active') === 'search') {
+    requestHandler = async (params) => {
+      await paymentsContainer.search({
+        params: {
+          por_pagina: paymentsContainer.get('search').get('pagination').options.perPage,
+          ...params
+        },
+        body: { ...searchForm.serialize() }
+      });
+    }
+  } else {
+    requestHandler = async (params) => {
+      await paymentsContainer.filter({
+        params: {
+          por_pagina: paymentsContainer.get('search').get('pagination').options.perPage,
+          ...params,
+        },
+        body: {
+          filters: { ...searchForm.serialize() },
+          subfilters: { ...tableRender.serializeTableFilters() }
+        }
+      });
+    }
+  }
+
+  return {
+    requestHandler,
+    params,
+    get: async function() {
+      await this.requestHandler(this.params);
+    }
+  }
+}
+
+tableRender.onFilter(async (filters) => {
+  const params = {
+    por_pagina: document.querySelector('#js-por-pagina').value,
+  };
+
+  paymentsContainer.toggleActiveData('filter');
+  if (Object.keys(filters).length === 0) {
+    paymentsContainer.toggleActiveData('search');
+    params.page = 1;
+  }
+
+  await buildRequest(params).get();
+});
+
+async function onPerPageChanged(event) {
+  paymentsContainer.get('search').get('pagination').setOptions({ perPage: event.target.value });
+  paymentsContainer.get('filtered').get('pagination').setOptions({ perPage: event.target.value });
+  await buildRequest({
+    page: 1,
+    por_pagina: event.target.value,
+  })
+  .get();
+}
+
+function exportar() {
+  swal('Aguarde um momento...', 'A sua planilha está sendo gerada.', 'warning');
+  setTimeout(() => {
+    openUrl(searchForm.get('form').dataset.urlExportar, {
+      ...searchForm.serialize(),
+      ...tableRender.serializeTableFilters(),
+    });
+  }, 500);
+}
+
+document.querySelector('input#data-inicial').setAttribute('disabled', true);
+
+document.querySelector('#js-por-pagina')
+  .addEventListener('change', onPerPageChanged);
+
+searchForm.get('form').querySelector('button[data-form-action="submit"')
+  .addEventListener('click', searchForm.get('onSubmitHandler'));
+
+document.querySelector('#js-exportar')
+  .addEventListener('click', exportar);
---
+++ Admin/resources/views/recebimentos/recebimentos-futuros.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.analytics-master')
+
+@section('headerStyle')
+  <link href="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{ URL::asset('assets/css/globals/global.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{ URL::asset('assets/css/teste.css')}}" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/recebimentos/pagina-recebimentos-futuros.css') }}" type="text/css">
+@endsection
+
+@section('content')
+  <main id="pagina-recebimentos-futuros" class="container-fluid">
+    <header>
+      @component('common-components.breadcrumb')
+        @slot('title') Recebimentos Futuros @endslot
+        @slot('item1') Recebimentos @endslot
+      @endcomponent
+    </header>
+
+    <div class="card">
+      <div class="card-body">
+        <x-forms.search-form
+          id="js-form-pesquisa"
+          :urls="[
+            ['buscar-recebimentos' => route('recebimentos-futuros.search')],
+            ['filtrar-recebimentos' => route('recebimentos-futuros.filter')],
+            ['exportar' => route('recebimentos-futuros.export')],
+          ]"
+          :hidden-fields="[
+            'estabelecimentos',
+            'status-conciliacao',
+            'status-financeiro',
+          ]"
+          :form-data="[
+            'data_inicial' => date('Y-m-d'),
+            'empresas' => $empresas,
+            'adquirentes' => $adquirentes,
+            'bandeiras' => $bandeiras,
+            'modalidades' => $modalidades,
+            'domicilios_bancarios' => $domicilios_bancarios,
+          ]"
+        />
+      </div>
+    </div>
+
+    <div class="resultados hidden">
+      <div class="boxes">
+        <x-box
+          title="BRUTO"
+          content="R$ 0,00"
+          data-format="currency"
+          data-key="TOTAL_BRUTO"
+          icon-path="assets/images/financeiro/bruto.svg"
+          icon-description="Valor Bruto"
+        />
+        <x-box
+          title="TAXA ADM."
+          content="-R$ 0,00"
+          content-class="text-danger"
+          data-format="currency"
+          data-key="TOTAL_TAXA"
+          icon-path="assets/images/financeiro/taxa-adm.svg"
+          icon-description="Taxa Adm."
+        />
+        <x-box
+          title="LÍQUIDO"
+          content="R$ 0,00"
+          data-format="currency"
+          data-key="TOTAL_LIQUIDO"
+          icon-path="assets/images/financeiro/liquido.svg"
+          icon-description="Valor Líquido"
+        />
+      </div>
+
+      <div class="content">
+        <div class="d-flex align-items-center justify-content-between">
+          <h4>Recebimentos Futuros <span id="js-quantidade-registros">(0 registros)</span></h4>
+          <div class="actions d-flex align-items-center justify-content-end">
+            <button id="js-exportar" class="btn button no-hover">
+              <i class="fas fa-file-download"></i>
+              Exportar
+            </button>
+          </div>
+        </div>
+
+        <x-tables.tabela-recebimentos-futuros
+          id="js-tabela-recebimentos"
+          class="mt-3"
+        >
+          <x-slot name="actions">
+            <td></td>
+          </x-slot>
+        </x-tables.tabela-recebimentos-futuros>
+        <x-tables.table-navigation
+          pagination-id="js-paginacao-recebimentos"
+          per-page-select-id="js-por-pagina"
+          :options="['10', '20', '50', '100', '200']"
+        />
+      </div>
+    </div>
+  </main>
+
+  <div id="js-loader" class="loader hidden"></div>
+@endsection
+
+@section('footerScript')
+  <script defer src="{{ URL::asset('assets/js/lib/api.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/formatter.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/pagination.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/modal-filters.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/checker.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/ui/table-render.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/ui/box.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/lib/ui/index.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/proxy/SearchFormProxy.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/proxy/PaymentsProxy.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/proxy/PaymentsContainerProxy.js') }}"></script>
+  <script defer src="{{ URL::asset('assets/js/recebimentos/recebimentos-futuros.js') }}"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+@endsection
