22ff1760392941573fb36beb8103548705b93987
vandermnt <vanderson.mantovani@hotmail.com>
2021-02-12 04:40:24 -0300

    style: centralizar texto box dash

--- Admin/resources/views/analytics/analytics-index.blade.php
+++ Admin/resources/views/analytics/analytics-index.blade.php
@@ -141,7 +141,7 @@ font-size: 14px;
                     </div>
                   </div>
                   <div class="col-12" style="margin-top: -20px">
-                    <h6 style="font-size: 12px" id="total_taxa_cobrada" class="my-3">Atendimento | Segunda a sexta-feira - das 08:00h às 18:00h</h6>
+                    <h6 style="font-size: 12px; text-align: center" id="total_taxa_cobrada" class="my-3">Atendimento | Segunda a sexta-feira - das 08:00h às 18:00h</h6>
                   </div>
 
                   <!-- <p class="mb-0 text-truncate"><span class="text-success"><i class="mdi mdi-trending-up"></i>8.5%</span> New Sessions Today</p> -->
