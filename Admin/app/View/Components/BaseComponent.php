8f34a16a1eaef2da2175dbd0cf7736f376f085b0
yuziem14 <yuri.fnunes@gmail.com>
2021-02-19 17:47:18 -0300

    chore: estende componente padrao do laravel

---
+++ Admin/app/View/Components/BaseComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class BaseComponent extends Component
+{
+    public $dataset;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($dataset = [])
+    {
+        $this->dataset = $dataset;
+    }
+
+    public function renderDataset() {
+        $dataAttributes = array_reduce(array_keys($this->dataset), function($datasetString, $key) {
+            return $datasetString."data-".$key."=\"".$this->dataset[$key]."\"\n";
+        });
+
+
+        return $dataAttributes ?? '';
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('components.base-component');
+    }
+}
