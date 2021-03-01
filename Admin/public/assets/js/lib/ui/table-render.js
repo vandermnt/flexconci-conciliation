2901ec5a31f67e9aeddfa44cb2278b4a4f7a5f0e
yuziem14 <yuri.fnunes@gmail.com>
2021-03-01 18:13:13 -0300

    chore: adiciona evento ao renderizar tabela

--- Admin/public/assets/js/lib/ui/table-render.js
+++ Admin/public/assets/js/lib/ui/table-render.js
@@ -1,7 +1,7 @@
 function TableRender(options = {}) {
   this.proxy = new Proxy({
     table: options.table || '#js-table',
-    data: options.data || { body: {}, footer: {} },
+    data: options.data || { body: [], footer: {} },
     formatter: options.formatter || new Formatter({
       locale: options.locale || 'en-US',
       currencyOptions: {
@@ -9,6 +9,7 @@ function TableRender(options = {}) {
       }
     }),
     selectedRows: [],
+    onRender: null,
     onRenderRow: () => {},
     onRenderCell: () => {},
     shouldSelectRow: () => true,
@@ -34,6 +35,10 @@ TableRender.prototype.formatCell = function(value, type = 'text', defaultValue =
     const formatedValue = formatter.format(type, value, defaultValue);
 
     return formatedValue;
+}
+
+TableRender.prototype.onRender = function(handler = (instance = this) => {}) {
+    this.set('onRender', handler);
 }
 
 TableRender.prototype.onRenderRow = function(handler = (row = null) => {}) {
@@ -100,6 +105,12 @@ TableRender.prototype.clearFilters = function() {
 }
 
 TableRender.prototype.render = function() {
+    const onRender = this.get('onRender');
+
+    if(onRender && typeof onRender === 'function') {
+        onRender(this);
+    }
+
     const table = this.get('table');
     const onRenderRow = this.get('onRenderRow');
     const onRenderCell = this.get('onRenderCell');
