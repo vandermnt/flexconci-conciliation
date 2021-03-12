6396ded1d8883daed7fd677f4ffc2b8231da45b5
yuziem14 <yuri.fnunes@gmail.com>
2021-03-12 15:57:23 -0300

    chore: adiciona dependencias de ordenacao a classe abstrata de filtros

--- Admin/app/Filters/BaseFilter.php
+++ Admin/app/Filters/BaseFilter.php
@@ -3,10 +3,33 @@
 namespace App\Filters;
 
 use App\Filters\Interfaces\BaseFilterInterface;
+use Illuminate\Support\Arr;
 
 abstract class BaseFilter implements BaseFilterInterface {
   protected $query = null;
   protected $whiteList = [];
+  protected $sortKeys = ['orderBy' => 'column', 'order' => 'direction'];
+
+  public function getSortKeys() {
+    return [$this->sortKeys['orderBy'], $this->sortKeys['order']];
+  }
+  
+  public function getAllowedKeys() {
+    return Arr::collapse([$this->whiteList, ['sort']]);
+  }
+
+  public function buildOrderClause($sortValues) {
+    if(!Arr::has($sortValues, $this->getSortKeys())) {
+      return $this->query;
+    }
+
+    $column = $sortValues[$this->sortKeys['orderBy']];
+    $direction = $sortValues[$this->sortKeys['order']] === 'asc' ? 'asc' : 'desc';
+
+    $this->query->orderBy($column, $direction);
+
+    return $this->query;
+  }
 
   public static function filter($filters) {}
 
