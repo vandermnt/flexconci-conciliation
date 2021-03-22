d61b602307a0817714e627136065b357c6065e80
yuziem14 <yuri.fnunes@gmail.com>
2021-03-22 15:59:48 -0300

    refactor: cria manipulador de erros de autorizacao cielo

--- Admin/app/EdiServices/Cielo/CieloEdiAuthorize.php
+++ Admin/app/EdiServices/Cielo/CieloEdiAuthorize.php
@@ -2,6 +2,10 @@
 
 namespace App\EdiServices\Cielo;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use App\Exceptions\EdiService\UnmatchStateException;
+use App\Exceptions\EdiService\AuthorizationDeniedException;
+use App\Exceptions\EdiService\EdiServiceException;
 
 class CieloEdiAuthorize {
   private $service = 'cielo';
@@ -26,6 +30,10 @@ class CieloEdiAuthorize {
     return config('ediservice.'.$this->service.'.base_url');
   }
 
+  public function generateState() {
+    return bin2hex(random_bytes(16));
+  }
+
   public function authenticate($params = []) {
     $queryParams = Arr::except($params, ['client_id', 'client_secret', 'mode']);
     $queryParams = Arr::collapse([
@@ -40,5 +48,35 @@ class CieloEdiAuthorize {
     $queryString = Arr::query($queryParams);
 
     return $this->getAuthUrl().'?'.$queryString;
+  }
+
+  public function handleAuthError($states, $error) {
+    $sessionState = $states['sessionState'];
+    $returnedState = $states['returnedState'];
+
+    throw_if($sessionState !== $returnedState, new UnmatchStateException('O estado entre requisições não são correspondentes.'));
+    throw_if($error === 'access_denied', new AuthorizationDeniedException('Acesso negado.'));
+    throw_if($error, new EdiServiceException('Um problema ocorreu. Tente novamente!'));
+  }
+
+  public function authorize($code) {
+    $response = Http::withHeaders([
+      'Content-Type' => 'application/json',
+      'Authorization' => $this->getAuthHeader(),
+    ])
+    ->post($this->getBaseUrl().'/consent/v1/oauth/access-token', [
+      'grant_type' => 'authorization_code',
+      'code' => $code
+    ]);
+
+    throw_if($response->failed(), new EdiServiceException('Autorização não concedida. Falha ao gerar chave de acesso!'));
+
+    $data = $response->json();
+    return [
+      'status' => 'success',
+      'data' => [
+        'access_token' => $data['access_token']
+      ]
+    ];
   }
 }
