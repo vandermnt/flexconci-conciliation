80283322a725b7bc0e0dd591ea144236aab2697e
matheuslk <matheus.augustolk@gmail.com>
2021-04-09 11:48:31 -0300

    mostra taxa.antecipacao=rec.operadoras

--- Admin/app/Filters/RecebimentosSubFilter.php
+++ Admin/app/Filters/RecebimentosSubFilter.php
@@ -6,85 +6,91 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use App\Filters\BaseSubFilter;
 
-class RecebimentosSubFilter extends BaseSubFilter {
-  protected $query = null;
-  protected $whiteList = [
-    'ID',
-    'DESCRICAO_ERP',
-    'NOME_EMPRESA',
-    'CNPJ',
-    'DATA_VENDA',
-    'DATA_PREVISAO',
-    'DATA_PAGAMENTO',
-    'ADQUIRENTE',
-    'BANDEIRA',
-    'MODALIDADE',
-    'TIPO_PAGAMENTO',
-    'NSU',
-    'AUTORIZACAO',
-    'TID',
-    'CARTAO',
-    'VALOR_BRUTO',
-    'TAXA_PERCENTUAL',
-    'VALOR_TAXA',
-    'VALOR_LIQUIDO',
-    'POSSUI_TAXA_MINIMA',
-    'PARCELA',
-    'TOTAL_PARCELAS',
-    'HORA',
-    'ESTABELECIMENTO',
-    'TERMINAL',
-    'BANCO',
-    'AGENCIA',
-    'CONTA',
-    'OBSERVACOES',
-    'PRODUTO',
-    'MEIOCAPTURA',
-    'STATUS_CONCILIACAO',
-    'STATUS_CONCILIADO',
-    'DIVERGENCIA',
-    'JUSTIFICATIVA',
-    'RETORNO_ERP_BAIXA'
-  ];
-  protected $numericFilters = [
-    'VALOR_BRUTO',
-    'TAXA_PERCENTUAL',
-    'VALOR_TAXA',
-    'VALOR_LIQUIDO',
-    'PARCELA',
-    'TOTAL_PARCELAS',
-  ];
+class RecebimentosSubFilter extends BaseSubFilter
+{
+	protected $query = null;
+	protected $whiteList = [
+		'ID',
+		'DESCRICAO_ERP',
+		'NOME_EMPRESA',
+		'CNPJ',
+		'DATA_VENDA',
+		'DATA_PREVISAO',
+		'DATA_PAGAMENTO',
+		'ADQUIRENTE',
+		'BANDEIRA',
+		'MODALIDADE',
+		'TIPO_PAGAMENTO',
+		'NSU',
+		'AUTORIZACAO',
+		'TID',
+		'CARTAO',
+		'VALOR_BRUTO',
+		'TAXA_PERCENTUAL',
+		'VALOR_TAXA',
+		'VALOR_LIQUIDO',
+		'TAXA_ANTECIPACAO',
+		'POSSUI_TAXA_MINIMA',
+		'PARCELA',
+		'TOTAL_PARCELAS',
+		'HORA',
+		'ESTABELECIMENTO',
+		'TERMINAL',
+		'BANCO',
+		'AGENCIA',
+		'CONTA',
+		'OBSERVACOES',
+		'PRODUTO',
+		'MEIOCAPTURA',
+		'STATUS_CONCILIACAO',
+		'STATUS_CONCILIADO',
+		'DIVERGENCIA',
+		'JUSTIFICATIVA',
+		'RETORNO_ERP_BAIXA'
+	];
+	protected $numericFilters = [
+		'VALOR_BRUTO',
+		'TAXA_PERCENTUAL',
+		'VALOR_TAXA',
+		'VALOR_LIQUIDO',
+		'PARCELA',
+		'TOTAL_PARCELAS',
+		'TAXA_ANTECIPACAO'
+	];
 
-  public static function subfilter($filters, $subfilters) {
-    $instance = app(RecebimentosSubFilter::class);
-    return $instance->apply($filters, $subfilters);
-  }
+	public static function subfilter($filters, $subfilters)
+	{
+		$instance = app(RecebimentosSubFilter::class);
+		return $instance->apply($filters, $subfilters);
+	}
 
-  public function apply($filters, $subfilters) {
-    $subfilters = Arr::only($subfilters, $this->whiteList);
-    $subfilters = Arr::where($subfilters, function($value, $key) {
-      return boolval($value);
-    });
+	public function apply($filters, $subfilters)
+	{
+		$subfilters = Arr::only($subfilters, $this->whiteList);
+		$subfilters = Arr::where($subfilters, function ($value, $key) {
+			return boolval($value);
+		});
 
-    $filterQuery = $this->getFilterQuery($filters);
+		$filterQuery = $this->getFilterQuery($filters);
 
-    $this->query = DB::table('recebimentos_sub')
-      ->select('*')
-      ->from(
-        DB::raw('('.$filterQuery->toSql().') as recebimentos_sub')
-      )
-      ->mergeBindings($filterQuery);
+		$this->query = DB::table('recebimentos_sub')
+			->select('*')
+			->from(
+				DB::raw('(' . $filterQuery->toSql() . ') as recebimentos_sub')
+			)
+			->mergeBindings($filterQuery);
 
-      foreach($subfilters as $subfilter => $value) {
-        $this->buildWhereClause($subfilter, $value);
-      }
+		foreach ($subfilters as $subfilter => $value) {
+			$this->buildWhereClause($subfilter, $value);
+		}
 
-    $this->buildOrderClause($filters['sort']);
+		$this->buildOrderClause($filters['sort']);
 
-    return $this;
-  }
+		return $this;
+	}
 
-  public function getQuery() {
-    return $this->query;
-  }
+	public function getQuery()
+	{
+		return $this->query;
+	}
 }
