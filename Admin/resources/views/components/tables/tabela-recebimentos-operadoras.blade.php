1b469e289b92ca9936dedc42de94880e90c2f0b9
matheuslk <matheus.augustolk@gmail.com>
2021-04-13 10:37:27 -0300

    correcao em taxa antec

--- Admin/resources/views/components/tables/tabela-recebimentos-operadoras.blade.php
+++ Admin/resources/views/components/tables/tabela-recebimentos-operadoras.blade.php
@@ -541,8 +541,8 @@
         <td data-column="VALOR_BRUTO" data-format="currency"></td>
         <td data-column="TAXA_PERCENTUAL" data-format="number"></td>
         <td class="text-danger" data-reverse-value="true" data-column="VALOR_TAXA" data-format="currency"></td>
-        <td data-reverse-value="true" data-column="TAXA_ANTECIPACAO" data-format="number"></td>
-				<td class="text-danger" data-column="VALOR_TAXA_ANTECIPACAO" data-format="number"></td>
+        <td data-column="TAXA_ANTECIPACAO" data-format="number"></td>
+				<td data-reverse-value="true" class="text-danger" data-column="VALOR_TAXA_ANTECIPACAO" data-format="number"></td>
         <td data-column="VALOR_LIQUIDO" data-format="currency"></td>
         <td data-column="POSSUI_TAXA_MINIMA"></td>
         <td data-column="PARCELA"></td>
@@ -602,7 +602,7 @@
         <td data-column="TOTAL_TAXA" data-reverse-value="true" data-format="currency" class="text-danger"></td>
         {{-- <td data-column="TOTAL_ANTECIPACAO" data-reverse-value="true" data-format="currency" class="text-danger"></td> --}}
         <td></td>
-				<td class="text-danger" data-column="TOTAL_VALOR_TAXA_ANTECIPACAO" data-format="currency"></td>
+				<td class="text-danger" data-reverse-value="true" data-column="TOTAL_VALOR_TAXA_ANTECIPACAO" data-format="currency"></td>
         <td data-column="TOTAL_LIQUIDO" data-format="currency"></td>
         <td></td>
         <td></td>
