642837e397dd78101645075ad611bd736d144228
matheuslk <matheus.augustolk@gmail.com>
2021-04-20 10:46:35 -0300

    add id_venda_erp filter rec.op

--- Admin/app/Filters/RecebimentosFilter.php
+++ Admin/app/Filters/RecebimentosFilter.php
@@ -126,8 +126,7 @@ class RecebimentosFilter extends BaseFilter
 		if (!is_null($recebimento_conciliado_erp) && count($recebimento_conciliado_erp) < 2) {
 			$filterValue = $recebimento_conciliado_erp[0];
 			$whereOperator = $filterValue === 'true' ? '!=' : "=";
-
-			// $this->query->where('ID_VENDA_ERP', $whereOperator, null);
+			$this->query->where('ID_VENDA_ERP', $whereOperator, NULL);
 		}
 		if (Arr::has($filters, 'domicilios_bancarios')) {
 			$this->query->whereIn('lista_bancos.CODIGO', function ($query) use ($filters) {
