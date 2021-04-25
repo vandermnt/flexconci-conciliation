1d5fcfd7d29b2c857766ec1ab94726bba3fedd2d
yuziem14 <yuri.fnunes@gmail.com>
2021-04-25 04:37:47 -0300

    chore: componentiza table config dropdown

---
+++ Admin/resources/views/components/table-config-dropdown.blade.php
@@ -0,0 +1,29 @@
+<div
+  id="{{ $attributes->get('id') }}"
+  class="table-config {{ $attributes->get('class') }}"
+  @php echo $renderDataset() @endphp
+>
+  <button class="table-config-control btn button no-hover">
+    <i class="fas fa-cog"></i>
+    Exibição de colunas
+  </button>
+  <div class="table-config-body">
+    <div class="table-config-list">
+      <div template class="table-config-option">
+        <input
+          type="checkbox"
+          data-group="{{ $attributes->get('checker-group') }}"
+          data-checker="global"
+          template
+        >
+        <span>Todos</span>
+      </div>
+    </div>
+    <div class="table-config-actions">
+      <button class="btn" data-action="confirm">
+        Confirmar
+        <i class="fa fa-check-circle"></i>
+      </button>
+    </div>
+  </div>
+</div>
