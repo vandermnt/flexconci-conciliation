3cadbfa671ba93f2738097829c587aead37c4ffd
yuziem14 <yuri.fnunes@gmail.com>
2021-04-25 16:29:03 -0300

    chore: adiciona historico local de exibicao das colunas

--- Admin/public/assets/js/lib/ui/table-config.js
+++ Admin/public/assets/js/lib/ui/table-config.js
@@ -4,8 +4,12 @@ function TableConfig(options) {
     rootElement: options.rootElement || '',
     sectionContainer: options.sectionContainer || new TableSectionContainer({ table: options.tableSelector || '' }),
     checkerGroup: options.checkerGroup || 'tb-config-columns',
-    visibility: {},
+    status: {
+      visible: [],
+      hidden: [],
+    },
     checker: new Checker(),
+    storageKey: options.storageKey || 'table_config'
   }, tableConfigHandler());
 }
 
@@ -21,23 +25,34 @@ TableConfig.prototype.set = function (prop = '', value = null) {
 	this.proxy[prop] = value;
 };
 
+TableConfig.prototype._generateStorageKey = function() {
+  const pathname = new URL(window.location.href).pathname.replaceAll('/', '@');
+  return `${pathname}${this.get('tableSelector')}.table_config`;
+}
+
+TableConfig.prototype._setup = function() {
+  this.set('storageKey', this._generateStorageKey());
+  const tableSectionContainer = new TableSectionContainer({
+    table: this.get('tableSelector'),
+  })
+    .buildSections();
+  this.set('sectionContainer', tableSectionContainer);
+  this.loadConfiguration();
+}
+
 TableConfig.prototype._registerEvents = function() {
   const tbConfigControl = this.get('rootElement').querySelector('.table-config-control');
   const confirmButton = this.get('rootElement').querySelector('button[data-action="confirm"]');
   if(confirmButton) {
-    confirmButton.addEventListener('click', (e) => {
-      const checkerGroup = this.get('checkerGroup');
-      const checker = this.get('checker');
-
-      this.show(checker.getCheckedValues(checkerGroup));
-      this.hide(checker.getUncheckedValues(checkerGroup));
-    });
+    confirmButton.addEventListener('click', this.onConfirm.bind(this));
     confirmButton.addEventListener('click', this.toggleDropdownMenu.bind(this));
   }
 
   if(tbConfigControl) {
     tbConfigControl.addEventListener('click', this.toggleDropdownMenu.bind(this));
   }
+
+  window.addEventListener('beforeunload', this.saveConfiguration.bind(this));
 }
 
 TableConfig.prototype.show = function(sections = []) {
@@ -56,13 +71,22 @@ TableConfig.prototype.toggleDropdownMenu = function() {
   rootElement.classList.toggle('has-focus');
 }
 
-TableConfig.prototype.init = function() {
-  const tableSectionContainer = new TableSectionContainer({
-    table: this.get('tableSelector'),
-  })
-    .buildSections();
-  this.set('sectionContainer', tableSectionContainer);
+TableConfig.prototype.onConfirm = function() {
+  const checkerGroup = this.get('checkerGroup');
+  const checker = this.get('checker');
+  const checkedValues = checker.getCheckedValues(checkerGroup);
+  const uncheckedValues = checker.getUncheckedValues(checkerGroup);
 
+  this.set('status', {
+    visible: checkedValues,
+    hidden: uncheckedValues
+  })
+  this.show(checkedValues);
+  this.hide(uncheckedValues);
+}
+
+TableConfig.prototype.init = function() {
+  this._setup();
   this.render();
   this._registerEvents();
   return this;
@@ -86,12 +110,32 @@ TableConfig.prototype.render = function() {
     optionTitle.textContent = section.title;
     checkbox.value = section.id;
     checkbox.dataset.checker = 'checkbox';
-    section.isVisible ? checkbox.checked = true : checkbox.removeAttribute('checked');
-
     tableConfigList.appendChild(configOption);
   });
 
-  this.get('checker').addGroup(this.get('checkerGroup'));
+  const checkerGroup = this.get('checkerGroup');
+  this.get('checker').addGroup(checkerGroup);
+  this.get('checker').checkOnly(checkerGroup, this.get('visibleSections'));
+}
+
+TableConfig.prototype.loadConfiguration = function() {
+  const savedConfiguration = JSON.parse(localStorage.getItem(this.get('storageKey')));
+  if(!savedConfiguration) {
+    this.set('status', {
+      visible: this.get('sectionContainer').getSectionsByVisibility({ mustBeVisible: true }).map(section => section.id),
+      hidden: this.get('sectionContainer').getSectionsByVisibility({ mustBeVisible: false }).map(section => section.id),
+    });
+
+    return;
+  };
+
+  this.set('status', savedConfiguration);
+  this.show(this.get('visibleSections'));
+  this.hide(this.get('hiddenSections'));
+}
+
+TableConfig.prototype.saveConfiguration = function() {
+  localStorage.setItem(this.get('storageKey'), JSON.stringify(this.get('status')));
 }
 
 function tableConfigHandler() {
@@ -107,6 +151,8 @@ function tableConfigHandler() {
 
 				return document.querySelector(target[name]);
 			}
+      if(name === 'visibleSections') return target.status.visible;
+      if(name === 'hiddenSections') return target.status.hidden;
 
 			return target[name];
 		},
