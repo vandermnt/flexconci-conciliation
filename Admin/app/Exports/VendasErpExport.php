b7e64fb498236a34291b029a94ba50830f993b3c
yuziem14 <yuri.fnunes@gmail.com>
2021-04-27 14:40:43 -0300

    chore: atualiza exportacao de vendas erp

--- Admin/app/Exports/VendasErpExport.php
+++ Admin/app/Exports/VendasErpExport.php
@@ -54,93 +54,18 @@ class VendasErpExport extends BaseExport implements FromQuery, WithStrictNullCom
     ];
 
     public function __construct($filters, $subfilters, $hidden = [], $dynamicHeaders = []) {
+      dd($filters, $subfilters, $hidden, $dynamicHeaders);
       parent::__construct($filters, $subfilters, $hidden, $dynamicHeaders);
     }
 
     public function headings(): array
     {
       return $this->getHeaders();
-        return [
-            'ID. ERP',
-            'Empresa',
-            'CNPJ',
-            'Venda',
-            'Previsão',
-            'Operadora',
-            'Bandeira',
-            'Forma de Pagamento',
-            'NSU',
-            'Autorização',
-            'TID',
-            'Cartão',
-            'Valor Bruto',
-            'Taxa %',
-            'Taxa R$',
-            'Valor Líquido',
-            'Parcela',
-            'Total Parcelas',
-            'Hora',
-            'Estabelecimento',
-            'Banco',
-            'Agencia',
-            'Conta',
-            'Produto',
-            'Meio de Captura',
-            'Status Conciliação',
-            'Divergência',
-            'Status Financeiro',
-            'Justificativa',
-            ucwords(mb_strtolower($this->dynamicHeaders->TITULO_CAMPO1 ?? 'Campo 1', 'utf-8')),
-            ucwords(mb_strtolower($this->dynamicHeaders->TITULO_CAMPO2 ?? 'Campo 2', 'utf-8')),
-            ucwords(mb_strtolower($this->dynamicHeaders->TITULO_CAMPO3 ?? 'Campo 3', 'utf-8')),
-            'Data Importação',
-            'Hora Importação',
-            'Data Conciliação',
-            'Hora Conciliação',
-        ];
     }
 
     public function map($venda): array
     {
       return $this->getValues($venda);
-        return [
-            $venda->DESCRICAO_ERP,
-            $venda->NOME_EMPRESA,
-            $venda->CNPJ." ",
-            is_null($venda->DATA_VENDA) ? null : date_format(date_create($venda->DATA_VENDA), 'd/m/Y'),
-            is_null($venda->DATA_VENCIMENTO) ? null : date_format(date_create($venda->DATA_VENCIMENTO), 'd/m/Y'),
-            $venda->ADQUIRENTE,
-            $venda->BANDEIRA,
-            $venda->MODALIDADE,
-            $venda->NSU." ",
-            $venda->CODIGO_AUTORIZACAO." ",
-            $venda->TID." ",
-            $venda->CARTAO." ",
-            $venda->VALOR_VENDA ?? 0,
-            $venda->TAXA ?? 0,
-            ($venda->VALOR_TAXA ?? 0) * -1,
-            $venda->VALOR_LIQUIDO_PARCELA ?? 0,
-            $venda->PARCELA,
-            $venda->TOTAL_PARCELAS,
-            $venda->HORA,
-            $venda->ESTABELECIMENTO." ",
-            $venda->BANCO,
-            $venda->AGENCIA." ",
-            $venda->CONTA_CORRENTE." ",
-            $venda->PRODUTO,
-            $venda->MEIOCAPTURA,
-            $venda->STATUS_CONCILIACAO,
-            $venda->DIVERGENCIA,
-            $venda->STATUS_FINANCEIRO,
-            $venda->JUSTIFICATIVA,
-            $venda->CAMPO1,
-            $venda->CAMPO2,
-            $venda->CAMPO3,
-            is_null($venda->DATA_IMPORTACAO) ? null : date_format(date_create($venda->DATA_IMPORTACAO), 'd/m/Y'),
-            $venda->HORA_IMPORTACAO,
-            is_null($venda->DATA_CONCILIACAO) ? null : date_format(date_create($venda->DATA_CONCILIACAO), 'd/m/Y'),
-            $venda->HORA_CONCILIACAO,
-        ];
     }
 
     public function query()
