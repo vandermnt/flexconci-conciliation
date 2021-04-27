793062c07901d4a513f88e939e9fc4405fa118b1
yuziem14 <yuri.fnunes@gmail.com>
2021-04-27 14:41:00 -0300

    chore: adiciona table config e table dragger em conciliacao de vendas

--- Admin/app/Exports/VendasErpConciliacaoExport.php
+++ Admin/app/Exports/VendasErpConciliacaoExport.php
@@ -10,108 +10,66 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
-class VendasErpConciliacaoExport implements FromQuery, WithStrictNullComparison, ShouldAutoSize, WithHeadings, WithMapping
+class VendasErpConciliacaoExport extends BaseExport implements FromQuery, WithStrictNullComparison, ShouldAutoSize, WithHeadings, WithMapping
 {
     use Exportable;
-    
-    protected $filters;
-    protected $subfilters;
 
-    public function __construct($filters, $subfilters) {
-        $this->filters = $filters;
-        $this->subfilters = $subfilters;
+    protected $keys = [
+      'DESCRICAO_ERP' => ['header' => 'ID. ERP', 'type' => 'string'],
+      'NOME_EMPRESA' => ['header' => 'Empresa', 'type' => 'string'],
+      'CNPJ' => ['header' => 'CNPJ', 'type' => 'forceToString'],
+      'DATA_VENDA' => ['header' => 'Venda', 'type' => 'date'],
+      'DATA_VENCIMENTO' => ['header' => 'Previsão', 'type' => 'date'],
+      'ADQUIRENTE' => ['header' => 'Operadora', 'type' => 'string'],
+      'BANDEIRA' => ['header' => 'Bandeira', 'type' => 'string'],
+      'MODALIDADE' => ['header' => 'Forma de Pagamento', 'type' => 'string'],
+      'NSU' => ['header' => 'NSU', 'type' => 'forceToString'],
+      'CODIGO_AUTORIZACAO' => ['header' => 'Autorização', 'type' => 'forceToString'],
+      'TID' => ['header' => 'TID', 'type' => 'forceToString'],
+      'VALOR_VENDA_PARCELA' => ['header' => 'Valor Bruto', 'type' => 'numeric'],
+      'VALOR_TAXA' => ['header' => 'Taxa R$', 'type' => 'numeric'],
+      'TAXA' => ['header' => 'Taxa %', 'type' => 'numeric'],
+      'TAXA_OPERADORA' => ['Taxa Op. %' => 'Taxa %', 'type' => 'numeric'],
+      'TAXA_DIFERENCA' => ['header' => 'Dif. Taxa %', 'type' => 'numeric'],
+      'VALOR_LIQUIDO_PARCELA' => ['header' => 'Valor Líquido', 'type' => 'numeric'],
+      'VALOR_LIQUIDO_OPERADORA' => ['header' => 'Valor Líquido Op.', 'type' => 'numeric'],
+      'DIFERENCA_LIQUIDO' => ['header' => 'Dif. Líquido R$', 'type' => 'numeric'],
+      'PARCELA' => ['header' => 'Parcela', 'type' => 'string'],
+      'TOTAL_PARCELAS' => ['header' => 'Total Parcelas', 'type' => 'string'],
+      'BANCO' => ['header' => 'Banco', 'type' => 'string'],
+      'AGENCIA' => ['header' => 'Agencia', 'type' => 'string'],
+      'CONTA_CORRENTE' => ['header' => 'Conta', 'type' => 'string'],
+      'PRODUTO' => ['header' => 'Produto', 'type' => 'string'],
+      'MEIOCAPTURA' => ['header' => 'Meio de Captura', 'type' => 'string'],
+      'STATUS_CONCILIACAO' => ['header' => 'Status Conciliação', 'type' => 'string'],
+      'DIVERGENCIA' => ['header' => 'Divergência', 'type' => 'string'],
+      'JUSTIFICATIVA' => ['header' => 'Justificativa', 'type' => 'string'],
+      'CAMPO1' => ['header' => 'Campo 1', 'type' => 'string'],
+      'CAMPO2' => ['header' => 'Campo 2', 'type' => 'string'],
+      'CAMPO3' => ['header' => 'Campo 3', 'type' => 'string'],
+      'DATA_IMPORTACAO' => ['header' => 'Data Importação', 'type' => 'date'],
+      'HORA_IMPORTACAO' => ['header' => 'Hora Importação', 'type' => 'string'],
+      'DATA_CONCILIACAO' => ['header' => 'Data Conciliação', 'type' => 'date'],
+      'HORA_CONCILIACAO' => ['header' => 'Hora Conciliação', 'type' => 'string'],
+    ];
+
+    public function __construct($filters, $subfilters, $hidden = [], $dynamicHeaders = []) {
+      parent::__construct($filters, $subfilters, $hidden, $dynamicHeaders);
     }
 
     public function headings(): array
     {
-        return [
-            'ID ERP',
-            'Empresa',
-            'CNPJ',
-            'Venda',
-            'Previsão',
-            'Operadora',
-            'Bandeira',
-            'Forma de Pagamento',
-            'NSU',
-            'Autorização',
-            'TID',
-            'Valor Bruto',
-            'Taxa R$',
-            'Taxa %',
-            'Taxa Op. %',
-            'Dif. Taxa %',
-            'Valor Líquido',
-            'Valor Líquido Op.',
-            'Dif. Líquido R$',
-            'Parcela',
-            'Total Parcelas',
-            'Banco',
-            'Agencia',
-            'Conta Corrente',
-            'Produto',
-            'Meio de Captura',
-            'Status Conciliação',
-            'Divergência',
-            'Status Financeiro',
-            'Justificativa',
-            'Campo 1',
-            'Campo 2',
-            'Campo 3',
-            'Data de Importação',
-            'Hora de Importação',
-            'Data de Conciliação',
-            'Hora de Conciliação',
-        ];
+      return $this->getHeaders();
     }
 
     public function map($venda): array
     {
-        return [
-            $venda->DESCRICAO_ERP,
-            $venda->NOME_EMPRESA,
-            $venda->CNPJ." ",
-            is_null($venda->DATA_VENDA) ? null : date_format(date_create($venda->DATA_VENDA), 'd/m/Y'),
-            is_null($venda->DATA_VENCIMENTO) ? null : date_format(date_create($venda->DATA_VENCIMENTO), 'd/m/Y'),
-            $venda->ADQUIRENTE,
-            $venda->BANDEIRA,
-            $venda->MODALIDADE,
-            $venda->NSU." ",
-            $venda->CODIGO_AUTORIZACAO." ",
-            $venda->TID." ",
-            ($venda->VALOR_VENDA_PARCELA ?? $venda->TOTAL_VENDA) ?? 0,
-            ($venda->VALOR_TAXA ?? 0) * -1,
-            $venda->TAXA ?? 0,
-            $venda->TAXA_OPERADORA ?? 0,
-            $venda->TAXA_DIFERENCA ?? 0,
-            $venda->VALOR_LIQUIDO_PARCELA ?? 0,
-            $venda->VALOR_LIQUIDO_OPERADORA ?? 0,
-            $venda->DIFERENCA_LIQUIDO ?? 0,
-            $venda->PARCELA,
-            $venda->TOTAL_PARCELAS,
-            $venda->BANCO,
-            $venda->AGENCIA,
-            $venda->CONTA_CORRENTE." ",
-            $venda->PRODUTO,
-            $venda->MEIOCAPTURA,
-            $venda->STATUS_CONCILIACAO,
-            $venda->DIVERGENCIA,
-            $venda->STATUS_FINANCEIRO,
-            $venda->JUSTIFICATIVA,
-            $venda->CAMPO1,
-            $venda->CAMPO2,
-            $venda->CAMPO3,
-            is_null($venda->DATA_IMPORTACAO) ? null : date_format(date_create($venda->DATA_IMPORTACAO), 'd/m/Y'),
-            is_null($venda->HORA_IMPORTACAO) ? null : date_format(date_create($venda->HORA_IMPORTACAO), 'H:m:s'),
-            is_null($venda->DATA_CONCILIACAO) ? null : date_format(date_create($venda->DATA_CONCILIACAO), 'd/m/Y'),
-            is_null($venda->HORA_CONCILIACAO) ? null : date_format(date_create($venda->HORA_CONCILIACAO), 'H:m:s'),
-        ];
+      return $this->getValues($venda);
     }
 
     public function query()
     {
         return VendasErpSubFilter::subfilter($this->filters, $this->subfilters)
-            ->getQuery()
-            ->orderBy('DATA_VENDA');
+            ->getQuery();
     }
 }
