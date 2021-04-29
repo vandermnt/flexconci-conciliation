64aca0fa1795adb186914951059bf41588fecc11
matheuslk <matheus.augustolk@gmail.com>
2021-04-29 11:53:20 -0300

    update about gerencial hint content

--- Admin/resources/views/common-components/breadcrumb.blade.php
+++ Admin/resources/views/common-components/breadcrumb.blade.php
@@ -1,28 +1,20 @@
 
 
 <div class="page-title-box breadcumb">
+	<div class="float-right">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">{{ $item1 }}</li>
+			<li class="breadcrumb-item active">{{ $title }}</li>
+		</ol>
+	</div>
 	@if(Request::path() == '/')
-		<div class="float-right">
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">{{ $item1 }}</li>
-				<!-- <li class="breadcrumb-item active"><a href="javascript:void(0);">{{ $item2 }}</a></li> -->
-				<li class="breadcrumb-item active">{{ $title }}</li>
-			</ol>
-		</div>
 		<div class="d-flex align-items-center">
 			<h4 class="page-title">{{$title}}</h4>
-			<div class="tooltip-hint about-gerencial" data-title="Entenda a tela gerencial !">
+			<div class="tooltip-hint about-gerencial" data-title="Clique aqui e entenda a tela gerencial">
 				<img class="ml-2" src="assets/images/widgets/youtube.png" alt="Entenda a tela gerencial !">
 			</div>
 		</div>
 	@else
-		<div class="float-right">
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">{{ $item1 }}</li>
-				<!-- <li class="breadcrumb-item active"><a href="javascript:void(0);">{{ $item2 }}</a></li> -->
-				<li class="breadcrumb-item active">{{ $title }}</li>
-			</ol>
-		</div>
 		<h4 class="page-title">{{ $title }}</h4>
 	@endif
-</div><!--end page-title-box-->
+</div>
