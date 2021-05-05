35848cc274dbad3c782f9ab7cbcf5e3bf6b7b6e3
matheuslk <matheus.augustolk@gmail.com>
2021-05-04 22:30:42 -0300

    remove controle_ajustes JOIN

--- Admin/app/Filters/RecebimentosFilter.php
+++ Admin/app/Filters/RecebimentosFilter.php
@@ -85,8 +85,8 @@ class RecebimentosFilter extends BaseFilter
 				'pagamentos_operadoras.RETORNO_BAIXA as RETORNO_ERP_BAIXA',
 				'pagamentos_operadoras.COD_TIPO_LANCAMENTO',
 				'pagamentos_operadoras.COD_AJUSTE',
-				'controle_ajustes.DESCRICAO_OPERADORA as DESC_AJUSTE',
-				'tipo_ajuste.TIPO_AJUSTE as CLASSIFICACAO_AJUSTE',
+				// 'controle_ajustes.DESCRICAO_OPERADORA as DESC_AJUSTE',
+				// 'tipo_ajuste.TIPO_AJUSTE as CLASSIFICACAO_AJUSTE',
 				'tipo_lancamento.TIPO_LANCAMENTO',
 
 			])
@@ -100,11 +100,11 @@ class RecebimentosFilter extends BaseFilter
 			->leftJoin('lista_bancos', 'lista_bancos.CODIGO', 'pagamentos_operadoras.COD_BANCO')
 			->leftJoin('meio_captura', 'meio_captura.CODIGO', 'pagamentos_operadoras.COD_MEIO_CAPTURA')
 			->leftJoin('status_conciliacao', 'status_conciliacao.CODIGO', 'pagamentos_operadoras.COD_STATUS')
-			->leftJoin('controle_ajustes', function ($join) {
-				$join->on('controle_ajustes.COD_ADQUIRENTE', '=', 'pagamentos_operadoras.COD_ADQUIRENTE');
-				$join->on('controle_ajustes.CODIGO_OPERADORA', '=', 'pagamentos_operadoras.COD_AJUSTE');
-			})
-			->leftJoin('tipo_ajuste', 'tipo_ajuste.CODIGO', 'controle_ajustes.COD_TIPO_AJUSTE_SISTEMA')
+			// ->leftJoin('controle_ajustes', function ($join) {
+			// 	$join->on('controle_ajustes.COD_ADQUIRENTE', '=', 'pagamentos_operadoras.COD_ADQUIRENTE');
+			// 	$join->on('controle_ajustes.CODIGO_OPERADORA', '=', 'pagamentos_operadoras.COD_AJUSTE');
+			// })
+			// ->leftJoin('tipo_ajuste', 'tipo_ajuste.CODIGO', 'controle_ajustes.COD_TIPO_AJUSTE_SISTEMA')
 			->where(
 				'pagamentos_operadoras.COD_CLIENTE',
 				$filters['cliente_id'],
