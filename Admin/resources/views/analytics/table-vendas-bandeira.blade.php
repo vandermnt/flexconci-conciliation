cb3c26b571947de20c417aa3d315dd3428b768eb
vandermnt <vanderson.mantovani@hotmail.com>
2021-05-18 00:49:44 -0300

    style: adicona imagem com a logo da Conciflex no pdf para baixar no dashboard

--- Admin/resources/views/analytics/table-vendas-bandeira.blade.php
+++ Admin/resources/views/analytics/table-vendas-bandeira.blade.php
@@ -1,46 +1,59 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <title>Conciflex - Dados do gráfico</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+    integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
   <!-- <link href="{{ URL::asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" /> -->
 </head>
+
 <body>
-  <h5 style="text-align: center"> Vendas por Bandeira </h5><br>
-  <div style="margin-left: -35px !important">
-    <table id="table_vendas_produto"  class="table" style="font-size: 12px">
-      <tr>
-        <th style="color: #231F20" >Bandeira
-          <th style="color: #231F20" >Quantidade</th>
-          <th style="color: #231F20" >Bruto</th>
-          <th style="color: #231F20" >Taxa</th>
-          <th style="color: #231F20" >Líquido</th>
-          <!-- <th style="color: #231F20" >Ticket Médio</th> -->
-        </tr>
-        <tbody>
-          @foreach($dados_vendas as $dado_venda)
-          @if($dado_venda->QUANTIDADE > 0)
-          <tr>
-            <td> {{ $dado_venda->BANDEIRA }}</td>
-            <td> {{ $dado_venda->QUANTIDADE_REAL }}</td>
-            <td> <?php $bruto =  number_format($dado_venda->TOTAL_BRUTO, 2, ',', '.'); ?> R$ {{ $bruto }}</td>
-            <td style="color: red"> <?php $taxa =  number_format($dado_venda->TOTAL_TAXA, 2, ',', '.'); ?> R$ {{ $taxa }}</td>
-            <td> <?php $liquido =  number_format($dado_venda->TOTAL_LIQUIDO, 2, ',', '.'); ?>R$ {{ $liquido }}</td>
-            <!-- <td> <?php $ticket =  number_format($dado_venda->TICKET_MEDIO, 2, ',', '.'); ?>R$ {{ $ticket }}</td> -->
-          </tr>
-          @endif
-          @endforeach
-        </tbody>
-        <tfoot style="font-weight: bolder">
-          <td> Totais </td>
-          <td> {{ $total['total_qtde'] }} </td>
-          <td> R$ <?php echo number_format($total['total_bruto'], 2, ',', '.'); ?></td>
-          <td style="color: red"> R$ <?php echo number_format($total['total_taxa'], 2, ',', '.'); ?></td>
-          <td> R$ <?php echo number_format($total['total_liquido'], 2, ',', '.'); ?></td>
-          <!-- <td> R$ <?php echo number_format($total['total_ticket'], 2, ',', '.'); ?></td> -->
-        </tfoot>
-      </table>
+  <div class="row">
+    <div class="col-sm-2">
+      <img src="../public/assets/images/logoconci.png" style="width: 150px;" alt="Logo Conciflex">
     </div>
-  </body>
-  </html>
+    <div class="col-sm-10">
+      <h5 style="text-align: center"> Vendas por Bandeira </h5><br>
+    </div>
+  </div>
+  </div>
+  <div style="margin-left: -35px !important">
+    <table id="table_vendas_produto" class="table" style="font-size: 12px">
+      <tr>
+        <th style="color: #231F20">Bandeira
+        <th style="color: #231F20">Quantidade</th>
+        <th style="color: #231F20">Bruto</th>
+        <th style="color: #231F20">Taxa</th>
+        <th style="color: #231F20">Líquido</th>
+        <!-- <th style="color: #231F20" >Ticket Médio</th> -->
+      </tr>
+      <tbody>
+        @foreach($dados_vendas as $dado_venda)
+        @if($dado_venda->QUANTIDADE > 0)
+        <tr>
+          <td> {{ $dado_venda->BANDEIRA }}</td>
+          <td> {{ $dado_venda->QUANTIDADE_REAL }}</td>
+          <td> <?php $bruto =  number_format($dado_venda->TOTAL_BRUTO, 2, ',', '.'); ?> R$ {{ $bruto }}</td>
+          <td style="color: red"> <?php $taxa =  number_format($dado_venda->TOTAL_TAXA, 2, ',', '.'); ?> R$ {{ $taxa }}
+          </td>
+          <td> <?php $liquido =  number_format($dado_venda->TOTAL_LIQUIDO, 2, ',', '.'); ?>R$ {{ $liquido }}</td>
+          <!-- <td> <?php $ticket =  number_format($dado_venda->TICKET_MEDIO, 2, ',', '.'); ?>R$ {{ $ticket }}</td> -->
+        </tr>
+        @endif
+        @endforeach
+      </tbody>
+      <tfoot style="font-weight: bolder">
+        <td> Totais </td>
+        <td> {{ $total['total_qtde'] }} </td>
+        <td> R$ <?php echo number_format($total['total_bruto'], 2, ',', '.'); ?></td>
+        <td style="color: red"> R$ <?php echo number_format($total['total_taxa'], 2, ',', '.'); ?></td>
+        <td> R$ <?php echo number_format($total['total_liquido'], 2, ',', '.'); ?></td>
+        <!-- <td> R$ <?php echo number_format($total['total_ticket'], 2, ',', '.'); ?></td> -->
+      </tfoot>
+    </table>
+  </div>
+</body>
+
+</html>
--- Admin/resources/views/analytics/table-vendas-produto.blade.php
+++ Admin/resources/views/analytics/table-vendas-produto.blade.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <title>Conciflex - Dados do gráfico</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+    integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
   <!-- <link href="{{ URL::asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" /> -->
 </head>
+
 <body>
-  <h5 style="text-align: center"> Vendas por Produto </h5><br>
+  <div class="row">
+    <div class="col-sm-2">
+      <img src="../public/assets/images/logoconci.png" style="width: 150px;" alt="Logo Conciflex">
+    </div>
+    <div class="col-sm-10">
+      <h5 style="text-align: center"> Vendas por Produto </h5><br>
+    </div>
+  </div>
   <div style="margin-left: -35px !important">
-    <table id="table_vendas_produto"  class="table" style="font-size: 12px">
+    <table id="table_vendas_produto" class="table" style="font-size: 12px">
       <thead>
         <tr>
-          <th style="color: #231F20" >Produto</th>
-          <th style="color: #231F20" >Quantidade</th>
-          <th style="color: #231F20" >Bruto</th>
-          <th style="color: #231F20" >Taxa</th>
-          <th style="color: #231F20" >Líquido</th>
+          <th style="color: #231F20">Produto</th>
+          <th style="color: #231F20">Quantidade</th>
+          <th style="color: #231F20">Bruto</th>
+          <th style="color: #231F20">Taxa</th>
+          <th style="color: #231F20">Líquido</th>
           <!-- <th style="color: #231F20" >Ticket Médio</th> -->
         </tr>
       </thead>
@@ -27,7 +37,8 @@
           <td> {{ $dado_venda->PRODUTO_WEB }}</td>
           <td> {{ $dado_venda->QUANTIDADE_REAL }}</td>
           <td> <?php $bruto =  number_format($dado_venda->TOTAL_BRUTO, 2, ',', '.'); ?> R$ {{ $bruto }}</td>
-          <td style="color: red"> <?php $taxa =  number_format($dado_venda->TOTAL_TAXA, 2, ',', '.'); ?> R$ {{ $taxa }}</td>
+          <td style="color: red"> <?php $taxa =  number_format($dado_venda->TOTAL_TAXA, 2, ',', '.'); ?> R$ {{ $taxa }}
+          </td>
           <td> <?php $liquido =  number_format($dado_venda->TOTAL_LIQUIDO, 2, ',', '.'); ?>R$ {{ $liquido }}</td>
           <!-- <td> <?php $ticket =  number_format($dado_venda->TICKET_MEDIO, 2, ',', '.'); ?>R$ {{ $ticket }}</td> -->
         </tr>
@@ -45,4 +56,5 @@
     </table>
   </div>
 </body>
+
 </html>
